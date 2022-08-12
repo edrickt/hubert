4a4ab34ab01c774bd05b445b381c97aa9757132f
edrickt <edrick.tamayo@zoho.com>
2022-08-11 20:20:38 -0400

    Code starting file

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import os
+import discord
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+infoFile = open("../info.txt", "r")
+TOKEN = infoFile.readline()
+client = discord.Client()
 
+@client.event
+async def on_ready():
+    print(f"{client.user} has connected to Discord")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+client.run(TOKEN)
